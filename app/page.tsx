7c14e6c72b27f9037ed8fcb7bb4e55b2e8706c59
Krasimir Kostadinov <krasimir.kostadinov.dev@gmail.com>
2023-09-22 21:47:49 +0300

    fixed error with the svg markup inside of tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Footer from "@/components/organisms/Footer";
 import IntroSection from "@/components/organisms/IntroSection";
 
 export default function Home() {
   return (
     <>
       <IntroSection />
+      <Footer />
     </>
   );
 }
