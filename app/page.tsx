4b642a354e77e336e2c5074d3b68a53110fc338f
Krasimir Kostadinov <krasimir.kostadinov.dev@gmail.com>
2023-10-01 10:04:09 +0300

    added ThreeImagesGallery

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import GallerySection from "@/components/organisms/GallerySection";
 import IntroSection from "@/components/organisms/IntroSection";
 import { Suspense } from "react";
 import Loading from "./loading";
+import ThreeImagesGallery from "@/components/organisms/ThreeImagesGallery";
 
 export default function Home() {
   return (
     <>
       <IntroSection />
+      <ThreeImagesGallery />
       <Suspense fallback={<Loading />}>
         <GallerySection />
       </Suspense>
---
+++ components/organisms/ThreeImagesGallery.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+
+export default function ThreeImagesGallery() {
+  return (
+    <section className="bg-neutral-800 py-3 ">
+      <div className="wrapper grid  md:grid-cols-3 md:h-[460px] gap-2">
+        <div className="w-full h-[380px] md:h-full relative">
+          <Image
+            src="/assets/bg-images/bg-1.jfif"
+            alt="1"
+            fill
+            objectFit="cover"
+          />
+        </div>
+        <div className="w-full h-[380px] md:h-full relative">
+          <Image
+            src="/assets/bg-images/bg-2.jpg"
+            alt="2"
+            fill
+            objectFit="cover"
+          />
+        </div>
+        <div className="w-full h-[380px] md:h-full relative">
+          <Image
+            src="/assets/bg-images/bg-3.jpg"
+            alt="3"
+            fill
+            objectFit="cover"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
