1e7cd2df393b3bf621c96186db5e646fe0a7669e
Krasimir Kostadinov <krasimir.kostadinov.dev@gmail.com>
2024-05-16 19:51:34 +0300

    some comments

--- app/control-panel/page.tsx
+++ app/control-panel/page.tsx
@@ -1,12 +1,21 @@
-import AddProductForm from "./components/AddProductForm";
+import AddProductForm from "./components/AddProductForm"
 
 export default function ControlPanelPage() {
   return (
     <div className="py-20">
       <h1 className="mb-10 text-center">Контролен Панел</h1>
+
+      {/* TODO:
+        Add a side pannel
+        add tabs for different content
+        add product | all products | account information | etc
+      
+      
+      */}
+
       <div className="grid place-items-center">
         <AddProductForm />
       </div>
     </div>
-  );
+  )
 }
